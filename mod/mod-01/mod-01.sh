c014199cb3f121fe17a60661b34edb637cc49cbb
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-05-26 15:09:28 -0500

    Added public to .gitignore; addition of minimal theme moved to Chapter 1

--- mod/mod-01/mod-01.sh
+++ mod/mod-01/mod-01.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 set -e
 
-echo '#########################'
-echo 'Chapter 1: Initial Commit'
-echo '#########################'
+echo '########################'
+echo 'Chapter 1: Minimal Theme'
+echo '########################'
 
 DIR_MAIN=$PWD
-
-echo '----------------------------------'
-echo 'Removing yarn.lock from .gitignore'
-sed -i -n "/.yarn.lock/{p; :a; N; //!ba; s/.*\n//}; p" .gitignore
-
-echo '------------------------------------'
-echo "cd $DIR_MAIN/website && yarn install"
-cd $DIR_MAIN/website && yarn install
 
 cd $DIR_MAIN && git init
 cd $DIR_MAIN && git add .
 cd $DIR_MAIN && git commit -m "Initial commit"
+
+echo '------------------------'
+echo 'Adding the minimal theme'
+cd $DIR_MAIN && git submodule add https://github.com/calintat/minimal.git themes/minimal
+cd $DIR_MAIN && git submodule init
+cd $DIR_MAIN && git submodule update
+cd $DIR_MAIN && git commit -m "Added minimal theme"
+
+# echo '-------'
+# echo 'hugo -D'
+# cd $DIR_MAIN && hugo -D
+
+# cd $DIR_MAIN && git init
+# cd $DIR_MAIN && git add .
+# cd $DIR_MAIN && git commit -m "Added minimal theme"
