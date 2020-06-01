c8a38abf8ab0ca408ff879341cfd77c929fb6313
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-06-01 00:10:33 -0500

    build-hugo points to README.txt; build.sh includes git submodule commands

--- mod/mod-02/mod-02-build.sh
+++ mod/mod-02/mod-02-build.sh
@@ -8,5 +8,13 @@ echo '******************************'
 echo 'FINISHED: docker-compose build'
 echo '******************************'
 
+echo '------------------'
+echo 'git submodule init'
+git submodule init
+
+echo '--------------------'
+echo 'git submodule update'
+git submodule update
+
 bash test_app.sh
 bash info.sh
